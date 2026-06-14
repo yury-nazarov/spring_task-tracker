3949dcec162b251476bffcb56bc37b289f76e9c0
Yury <yury.s.nazarov@gmail.com>
2026-06-14 19:12:06 +0300

    feature: TaskController - добавил основные ручки с моками и логирование по ним

--- src/main/java/homework/task_tracker/controller/TaskController.java
+++ src/main/java/homework/task_tracker/controller/TaskController.java
@@ -5,13 +5,14 @@ import homework.task_tracker.service.TaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/task")
 public class TaskController {
 
     public static final Logger log = LoggerFactory.getLogger(TaskController.class);
@@ -23,15 +24,44 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @GetMapping("/task/{id}")
-    public Task getTaskById(@PathVariable("id") Long id) {
-        log.info("Called getTaskById: id = {}", id);
-        return taskService.getTaskById(id);
+    @GetMapping("/list")
+    public ResponseEntity<List<Task>> getAllTask() {
+        log.info("Called getAllTask");
+        return ResponseEntity.ok(taskService.getAllTasks());
     }
 
-    @GetMapping("/")
-    public List<Task> getAllTask() {
-        log.info("Called getAllTask");
-        return taskService.getAllTasks();
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable("id") Long id) {
+        log.info("Called getTaskById: id = {}", id);
+        return ResponseEntity.ok(taskService.getTaskById(id));
     }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(
+            @RequestBody Task task
+    ){
+        log.info("Called createTask");
+        // TODO
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask (
+            @PathVariable Long id,
+            @RequestBody Task task
+    ) {
+        log.info("Called updateTask: id = {}", id);
+        // TODO
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask (
+            @PathVariable Long id
+    ) {
+        log.info("Called deleteTask: id = {}", id);
+        // TODO
+        return ResponseEntity.ok().build();
+    }
+
 }
